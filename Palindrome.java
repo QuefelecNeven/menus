944c7926b3dedb0869864917a0790e0d4b586121
Joaquin "Joe" Lapin <Jo.Lapin@univ-hochmoehrefelder.fr>
2026-01-29 11:09:47 +0100

    reprise du git

---
+++ Palindrome.java
@@ -0,0 +1,13 @@
+public class Palindrome {
+    private String mot; 
+
+
+
+    public boolean isPalindrome(){
+        fot (int i = 0 ; i < palindrome.getTaille(); i++){
+            if (i.getLettre().equals(-i.getLettre()));
+            return true;
+        }
+        return false ;
+    }
+}
--- src/main/java/fr/univ_orleans/iut45/menus/Menu.java
+++ src/main/java/fr/univ_orleans/iut45/menus/Menu.java
@@ -8,8 +8,8 @@ class Menu {
     protected List<Plat> desserts;
 
     public Menu(List<Plat> entrees,
-                List<Plat> plats_principaux,
-                List<Plat> desserts) {
+            List<Plat> plats_principaux,
+            List<Plat> desserts) {
         this.entrees = entrees;
         this.plats_principaux = plats_principaux;
         this.desserts = desserts;
@@ -25,5 +25,52 @@ class Menu {
 
     public List<Plat> getDesserts() {
         return this.desserts;
+    }
+
+    public double getPrixRevientMin() {
+        double minE = entrees.get(0).prixDeRevient();
+        for (Plat p : entrees) {
+
+            if (p.prixDeRevient() < minE)
+                minE = p.prixDeRevient();
+
+        }
+        double minP = plats_principaux.get(0).prixDeRevient();
+        for (Plat p : plats_principaux) {
+            if (p.prixDeRevient() < minP)
+                minP = p.prixDeRevient();
+        }
+        double minD = desserts.get(0).prixDeRevient();
+        for (Plat p : desserts) {
+
+            if (p.prixDeRevient() < minD)
+                minD = p.prixDeRevient();
+
+        }
+        return minE + minP + minD;
+    }
+
+    public double getPrixRevientMax() {
+        double maxE = entrees.get(0).prixDeRevient();
+        for (Plat p : entrees) {
+            if (p.prixDeRevient() > maxE)
+                maxE = p.prixDeRevient();
+
+        }
+        double maxP = plats_principaux.get(0).prixDeRevient();
+        for (Plat p : plats_principaux) {
+            if (p.prixDeRevient() > maxP)
+                maxP = p.prixDeRevient();
+        }
+
+        double maxD = desserts.get(0).prixDeRevient();
+
+        for (Plat p : desserts) {
+            if (p.prixDeRevient() > maxD)
+                maxD = p.prixDeRevient();
+        }
+
+        return maxE + maxP + maxD;
+
     }
 }
